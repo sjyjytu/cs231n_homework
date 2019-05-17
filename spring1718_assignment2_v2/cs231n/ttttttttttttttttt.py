573a8599a2d415e57f151cc5f5239ba6ed4439e5
kty <1332372004@qq.com>
2019-05-17 13:03:02 +0800

    update pytorch

--- spring1718_assignment2_v2/cs231n/ttttttttttttttttt.py
+++ spring1718_assignment2_v2/cs231n/ttttttttttttttttt.py
@@ -1,21 +1,184 @@
 import torch
-import numpy
-a = numpy.array([[1,2],[3,4]])
-# print('numpy:')
-# print('.dot:')
-# print(a.dot(a))
-# print('.matmul:')
-# print(numpy.matmul(a,a))
-# print('*')
-# print(a*a)
-# print('torch:')
-b = torch.randn((2,4))
-c = torch.randn((2,))
-# print(b)
-# print('dot:')
-# print(b.mul(b))
-# print('mm:')
-# print(b.mm(b))
-print(b)
-print(c)
-print(b+c)
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+import torch.nn.functional as F  # useful stateless functions
+
+import numpy as np
+
+NUM_TRAIN = 49000
+
+# The torchvision.transforms package provides tools for preprocessing data
+# and for performing data augmentation; here we set up a transform to
+# preprocess the data by subtracting the mean RGB value and dividing by the
+# standard deviation of each RGB value; we've hardcoded the mean and std.
+transform = T.Compose([
+                T.ToTensor(),
+                T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+            ])
+
+# We set up a Dataset object for each split (train / val / test); Datasets load
+# training examples one at a time, so we wrap each Dataset in a DataLoader which
+# iterates through the Dataset and forms minibatches. We divide the CIFAR-10
+# training set into train and val sets by passing a Sampler object to the
+# DataLoader telling how it should sample from the underlying Dataset.
+cifar10_train = dset.CIFAR10('./datasets', train=True, download=True,
+                             transform=transform)
+loader_train = DataLoader(cifar10_train, batch_size=64,
+                          sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN)))
+
+cifar10_val = dset.CIFAR10('./cs231n/datasets', train=True, download=True,
+                           transform=transform)
+loader_val = DataLoader(cifar10_val, batch_size=64,
+                        sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN, 50000)))
+
+cifar10_test = dset.CIFAR10('./cs231n/datasets', train=False, download=True,
+                            transform=transform)
+loader_test = DataLoader(cifar10_test, batch_size=64)
+
+USE_GPU = True
+
+dtype = torch.float32 # we will be using float throughout this tutorial
+
+if USE_GPU and torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+# Constant to control how frequently we print train loss
+print_every = 100
+
+print('using device:', device)
+
+def flatten(x):
+    N = x.shape[0] # read in N, C, H, W
+    return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+
+def test_flatten():
+    x = torch.arange(12).view(2, 1, 3, 2)
+    print('Before flattening: ', x)
+    print('After flattening: ', flatten(x))
+
+def check_accuracy_part34(loader, model):
+    if loader.dataset.train:
+        print('Checking accuracy on validation set')
+    else:
+        print('Checking accuracy on test set')
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
+        return acc
+
+def train_part34(model, optimizer, epochs=1):
+    """
+    Train a model on CIFAR-10 using the PyTorch Module API.
+
+    Inputs:
+    - model: A PyTorch Module giving the model to train.
+    - optimizer: An Optimizer object we will use to train the model
+    - epochs: (Optional) A Python integer giving the number of epochs to train for
+
+    Returns: Nothing, but prints model accuracies during training.
+    """
+    model = model.to(device=device)  # move the model parameters to CPU/GPU
+    acc = 0
+    for e in range(epochs):
+        for t, (x, y) in enumerate(loader_train):
+            model.train()  # put model to training mode
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+
+            scores = model(x)
+            loss = F.cross_entropy(scores, y)
+
+            # Zero out all of the gradients for the variables which the optimizer
+            # will update.
+            optimizer.zero_grad()
+
+            # This is the backwards pass: compute the gradient of the loss with
+            # respect to each  parameter of the model.
+            loss.backward()
+
+            # Actually update the parameters of the model using the gradients
+            # computed by the backwards pass.
+            optimizer.step()
+
+            if t % print_every == 0:
+                print('Iteration %d, loss = %.4f' % (t, loss.item()))
+                acc = check_accuracy_part34(loader_val, model)
+                print()
+    return acc
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return flatten(x)
+
+model = None
+optimizer = None
+channel_1 = 32
+channel_2 = 16
+channel_3 = 8
+
+models = [
+    nn.Sequential(
+    nn.Conv2d(3, 48, 7, 1, 3),
+    nn.ReLU(),
+    nn.Conv2d(48, 32, 5, 1, 2),
+    nn.ReLU(),
+    nn.Conv2d(32, 16, 3, 1, 1),
+    nn.ReLU(),
+    Flatten(),
+    nn.Linear(16*32*32, 10)
+),
+    nn.Sequential(
+    nn.Conv2d(3, channel_1, 5, 1, 2),
+    nn.ReLU(),
+    nn.MaxPool2d(2,2),
+    nn.Conv2d(channel_1, channel_2, 3, 1, 1),
+    nn.ReLU(),
+    nn.MaxPool2d(2, 2),
+    Flatten(),
+    nn.Linear(channel_2*8*8, 30),
+    nn.ReLU(),
+    nn.Linear(30, 10),
+),
+    nn.Sequential(
+    nn.Conv2d(3, channel_1, 5, 1, 2),
+    nn.ReLU(),
+    nn.Conv2d(channel_1, channel_2, 4, 2, 1),
+    nn.ReLU(),
+    nn.Conv2d(channel_2, 16, 4, 3, 0),
+    nn.ReLU(),
+    Flatten(),
+    nn.Linear(16*4*4, 10)
+),
+]
+optimizers = [optim.SGD(models[i].parameters(), lr=3e-3,
+                     momentum=0.9, nesterov=True) for i in range(len(models))]
+best_acc = 0
+for i in range(len(models)):
+    acc = train_part34(models[i], optimizers[i], epochs=2)
+    if acc > best_acc:
+        best_acc = acc
+        model = models[i]
+        optimizer = optimizers[i]
+################################################################################
+#                                 END OF YOUR CODE
+################################################################################
+
+# You should get at least 70% accuracy
+train_part34(model, optimizer, epochs=10)
