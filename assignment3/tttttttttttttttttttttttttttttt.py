84a9b0810c2c4b9584493c43f3b823770e82012a
kty <1332372004@qq.com>
2019-05-17 12:53:29 +0800

    finish GAN

--- assignment3/tttttttttttttttttttttttttttttt.py
+++ assignment3/tttttttttttttttttttttttttttttt.py
@@ -1,8 +1,28 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-a = np.zeros((3,4))
-b = np.ones((3, ))
-for i in range(a.shape[1]):
-    a[:,i] = b
-    b += b
-print(a)
+def f(x,y):
+    return x*y
+
+num_iter = 6
+lr = 1
+x = 1.0
+y = 1.0
+fs = [1.0,]
+xs = [x,]
+ys = [y,]
+for i in range(num_iter):
+    y += lr * x
+    x -= lr * y
+    xs.append(x)
+    ys.append(y)
+    fs.append(f(x, y))
+
+iter = [i+1 for i in range(num_iter+1)]
+plt.plot(iter, xs,'g--', label='x')
+plt.plot(iter, ys,'r--', label='y')
+plt.plot(iter, fs,'b--', label='f')
+plt.legend()
+print(xs)
+print(ys)
+plt.show()
